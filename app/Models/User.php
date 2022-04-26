0847a4cc53d7379f529d1a8cc6167848975b82b4
rfttra <auliafitra2002@gmail.com>
2022-04-26 23:39:30 +0700

    Practicum 2

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function todo(){
+        return $this->hasMany(Todo::class);
+    }
 }
